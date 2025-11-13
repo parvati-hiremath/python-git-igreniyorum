10f35be0389d6f4d9eaa0d46ac460ffa8271df28
parvati-hiremath <parvatihiremath2024@outlook.com>
2025-11-13 09:04:54 +0000

    Added division function!

--- hesap_machinesi.py
+++ hesap_machinesi.py
@@ -1,5 +1,6 @@
 a = int(input("Enter the first number: "))
 b = int(input("Enter the second number: "))
 product = a * b
-
+Division = a / b
+print(f"The division of {a} by {b} is: {Division}")
 print(f"The product of {a} and {b} is: {product}")
