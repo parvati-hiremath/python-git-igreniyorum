92876b8f371dbaa7d7a2863af2b14726658b39d4
parvati-hiremath <parvatihiremath2024@outlook.com>
2025-11-13 03:35:26 +0000

    simple calculator program added

---
+++ calculator.py
@@ -0,0 +1,16 @@
+
+def Addition(a, b):
+  return  a + b
+
+def Subtraction(c, d):
+  return c - d
+
+a = 10
+b = 5
+
+
+sum = Addition(a, b)
+print(f"The sum of {a} and {b} is: {sum}")
+
+sub = Subtraction(a, b)
+print(f"The difference between {a} and {b} is: {sub}")
